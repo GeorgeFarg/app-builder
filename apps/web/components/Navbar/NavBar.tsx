4f99649352085f93f9487938356f4ddace3821bf
George Farg <georgefarg7@gmail.com>
2025-08-09 11:46:55 +0300

    feat: replace button with RippleButton component in NavBar; add RippleButton implementation

--- apps/web/components/Navbar/NavBar.tsx
+++ apps/web/components/Navbar/NavBar.tsx
@@ -1,3 +1,4 @@
+import RippleButton from "@repo/ui/rippleButton";
 import Image from "next/image";
 import { FaChevronDown } from "react-icons/fa";
 
@@ -5,12 +6,12 @@ const NavBar = () => {
   return (
     <div className='dark:bg-dark-main text-white h-[60px] w-full border-b-2 border-b-dark-light-300 px-[30px]'>
       <div className='flex justify-between items-center h-full'>
-        <button className='flex px-[8px] py-[6px] dark:bg-dark-light-300 rounded-[10px] '>
+        <RippleButton className='flex px-[8px] py-[6px] dark:bg-dark-light-300 rounded-[10px]'>
           <Image width={30} height={30} src={"/logo.svg"} alt='logo' />
           <div className='w-[30px] h-[30px] flex items-center justify-center'>
             <FaChevronDown color='#7C7C7C' className='center' />
           </div>
-        </button>
+        </RippleButton>
       </div>
     </div>
   );
