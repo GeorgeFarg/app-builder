b93f77823bea2339d8230e2175732755ba5fb4e1
GeorgeFarg <georgefarg7@gmail.com>
2025-11-10 16:57:32 +0200

    refactor(NavBar): adjust styling and layout for improved appearance

--- apps/web/components/Navbar/NavBar.tsx
+++ apps/web/components/Navbar/NavBar.tsx
@@ -4,28 +4,28 @@ import { FaChevronDown } from "react-icons/fa";
 
 const NavBar = () => {
   return (
-    <div className='dark:bg-dark-main text-white h-[60px] w-full border-b-2 border-b-dark-light-300 px-[30px]'>
-      <div className='flex justify-between items-center h-full'>
-        <RippleButton className='flex px-[8px] py-[6px] dark:bg-dark-light-300 rounded-[10px] cursor-pointer'>
-          <Image width={30} height={30} src={"/logo.svg"} alt='logo' />
-          <div className='w-[30px] h-[30px] flex items-center justify-center'>
-            <FaChevronDown color='#7C7C7C' className='center' />
+    <div className="dark:bg-dark-main text-white h-[50px] w-full border-b-2 border-b-dark-light-300 px-[30px]">
+      <div className="flex justify-between items-center h-full">
+        <RippleButton className="flex px-[8px] h-[38px] py-[6px] dark:bg-dark-light-300 rounded-[10px] cursor-pointer">
+          <Image width={25} height={25} src={"/logo.svg"} alt="logo" />
+          <div className="w-[25px] h-[25px] flex items-center justify-center">
+            <FaChevronDown color="#7C7C7C" className="center" />
           </div>
         </RippleButton>
 
-        <div className='h-full flex py-[8px] gap-1'>
-          <div className='dark:bg-dark-light-300 px-[12px] py-[10px] rounded-l-[8px]'>
-            <p className='text-[20px] relative centralize'>Untitled</p>
+        <div className="h-full flex py-[8px] gap-1">
+          <div className="dark:bg-dark-light-300 px-[12px] py-[10px] rounded-l-[8px]">
+            <p className="text-[16px] relative centralize">Untitled</p>
           </div>
-          <RippleButton className='p-3 dark:bg-dark-light-300 rounded-r-[8px] cursor-pointer'>
+          <RippleButton className="px-3 py-2 items-center dark:bg-dark-light-300 rounded-r-[8px] cursor-pointer">
             <FaChevronDown
-              color='#7C7C7C relative centralize'
-              className='center'
+              color="#7C7C7C relative centralize"
+              className="center"
             />
           </RippleButton>
         </div>
 
-        {/* Account button section */}
+        {/* Account button section --- Yet to implement */}
         <div></div>
       </div>
     </div>
