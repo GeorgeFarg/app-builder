f0103e9944471aae001709dc77b8843a8f983aa4
GeorgeFarg <georgefarg7@gmail.com>
2025-11-25 01:18:32 +0200

    feat(editor): add draggable component to ElementsSideBar for enhanced UI interaction

--- apps/web/components/Editor/ElementsSideBar.tsx
+++ apps/web/components/Editor/ElementsSideBar.tsx
@@ -1,5 +1,23 @@
+import { EditorBtns } from "@/types/Editor";
+import { TextCursor } from "lucide-react";
+import React from "react";
+
+
 const ElementsSideBar = () => {
-  return <aside className='max-w-[150px]'>CompnentsSideBar</aside>;
+
+  const handleDrag = (e: React.DragEvent, type: EditorBtns) => {
+    if (type === null) return
+    e.dataTransfer.setData('componentType', type)
+  }
+
+  return <aside className='max-w-[150px]'>
+    <div
+      draggable
+      className="w-20 h-20 "
+    >
+      <TextCursor />
+    </div>
+  </aside>;
 };
 
 export default ElementsSideBar;
